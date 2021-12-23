75e846379942b23f1642d3b62d10a40b8d7f8ccc
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:28:09 -0300

    importando components pra home page

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,12 +1,20 @@
 import React, { useContext } from 'react';
+import Header from '../components/Header';
+import FOTO2 from '../FOTO2.png';
+import CardsHardSkills from  '../components/CardsHardSkills';
+import { VscTools } from "react-icons/vsc";
+import Footer from '../components/Footer';
+import PortfolioContext from '../context/PortfolioContext';
 import '../css/App.css';
 
 export default function Home() {
   const { themePortfolio } = useContext(PortfolioContext);
   console.log(themePortfolio);
     return (
-        <body>
-     
+        <body className={ themePortfolio ? "body-ligth" : "body-dark"}>
+          <Header />
+          <CardsHardSkills />
+          <Footer/>
         </body>
     );
 }
