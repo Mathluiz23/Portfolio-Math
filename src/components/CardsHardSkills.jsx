f8028febcbb416caf5962c9d8697edb4e088f0b3
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:13:41 -0300

    add component de cards para hardskills na page home

---
+++ src/components/CardsHardSkills.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/App.css';
+
+export default function CardHardSkills() {
+
+  return (
+    <>
+      <div className='hardSkills-container'>
+
+        <div>
+          <h2>React</h2>
+        </div>
+      </div>
+    </>
+  );
+}
