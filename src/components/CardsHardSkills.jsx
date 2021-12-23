de38e9849de4666ea7c4705d9934a3eb268c182d
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:14:26 -0300

    add context ao component card

--- src/components/CardsHardSkills.jsx
+++ src/components/CardsHardSkills.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import PortfolioContext from '../context/PortfolioContext';
 import '../css/App.css';
 
 export default function CardHardSkills() {
-
+  const { themePortfolio } = useContext(PortfolioContext);
   return (
     <>
       <div className='hardSkills-container'>
 
-        <div>
+        <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
           <h2>React</h2>
         </div>
       </div>
