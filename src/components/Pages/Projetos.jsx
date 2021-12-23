46f935ab8145a28303c1d6047d4abb10bed407f4
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:31:03 -0300

    inserindo context na projetos page

--- src/components/Pages/Projetos.jsx
+++ src/components/Pages/Projetos.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PortfolioContext from '../context/PortfolioContext';
 import '../css/App.css';
 
 
 export default function Projetos() {
-  
+  const { themePortfolio } = useContext(PortfolioContext);
     return (
         <>
-          <body>
+          <body className={ themePortfolio ? "body-ligth" : "body-dark"}>
            
           </body>
         </>
