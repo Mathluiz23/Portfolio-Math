87176be58edc9fad000ce3fdf70a4c86aca6badd
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:23:05 -0300

    add locacao para inserir projetos futuros e classes

--- src/components/ProjetosList.jsx
+++ src/components/ProjetosList.jsx
@@ -5,11 +5,11 @@ import '../css/App.css';
 export default function ProjetosList() {
     const { themePortfolio} = useContext(PortfolioContext);
     return (
-        <div>
+        <div className={ themePortfolio ? "container-ligth" : "container-dark" }>
 
           <div className="card-container">
             <div className="card">
-              <div>
+              <div className="card-wrapper">
                 <h2>Projeto 1</h2>
                 <p>Vem ver!</p>
               </div>
@@ -19,6 +19,55 @@ export default function ProjetosList() {
               Aqui contém um projeto realizado na Trybe, no curso de desenvolvimento Web. Colocando em prática os conhecimentos adquiridos.
             </div> 
           </div>
+
+          <div className="card-container">
+            <div className="card">
+              <div className="card-wrapper">
+                <h2>Projeto 2</h2>
+                <p>Vem ver!</p>
+              </div>
+            </div>
+            <div className={ themePortfolio ? "card-text-ligth" : "card-text-dark"}>
+              Aqui contém um projeto realizado na Trybe, no curso de desenvolvimento Web. Colocando em prática os conhecimentos adquiridos.
+            </div> 
+          </div>
+
+          <div className="card-container">
+            <div className="card">
+              <div className="card-wrapper">
+                <h2>Projeto 3</h2>
+                <p>Vem ver!</p>
+              </div>
+            </div>
+            <div className={ themePortfolio ? "card-text-ligth" : "card-text-dark"}>
+              Aqui contém um projeto realizado na Trybe, no curso de desenvolvimento Web. Colocando em prática os conhecimentos adquiridos.
+            </div> 
+          </div>
+
+          <div className="card-container">
+            <div className="card">
+              <div className="card-wrapper">
+                <h2>Projeto 4</h2>
+                <p>Vem ver!</p>
+              </div>
+            </div>
+            <div className={ themePortfolio ? "card-text-ligth" : "card-text-dark"}>
+              Aqui contém um projeto realizado na Trybe, no curso de desenvolvimento Web. Colocando em prática os conhecimentos adquiridos.
+            </div> 
+          </div>
+
+          <div className="card-container">
+            <div className="card">
+              <div className="card-wrapper">
+                <h2>Projeto 5</h2>
+                <p>Vem ver!</p>
+              </div>
+            </div>
+            <div className={ themePortfolio ? "card-text-ligth" : "card-text-dark"}>
+              Aqui contém um projeto realizado na Trybe, no curso de desenvolvimento Web. Colocando em prática os conhecimentos adquiridos.
+            </div> 
+          </div>
+
         </div> 
     );
 }
