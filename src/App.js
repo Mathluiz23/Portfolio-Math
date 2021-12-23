ae3e032b2eedca2517ec7e0b068cea9cbe9aa0e6
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:39:05 -0300

    inserindo as rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Route, Routes} from 'react-router-dom';
+import Home from './Pages/Home';
+import Projetos from './Pages/Projetos';
+import PortfolioProvider from './context/PortfolioProvider';
+import '../src/css/App.css';
+
 
 function App() {
   return (
     <div>
+        <PortfolioProvider>
           <Routes>
-            <Route />
-            <Route />
+            <Route path="/projetos" element={ <Projetos /> } />
+            <Route exatc path="/" element={< Home />} />
           </Routes>
+        </PortfolioProvider>
 
     </div>
   );
