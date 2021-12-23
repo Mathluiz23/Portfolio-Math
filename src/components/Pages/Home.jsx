f15e279c4b397e557a490f94f67a047a856a41d9
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:28:58 -0300

    inserindo informacoes e logica css na home page

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -13,7 +13,29 @@ export default function Home() {
     return (
         <body className={ themePortfolio ? "body-ligth" : "body-dark"}>
           <Header />
+          <div className={ themePortfolio ? "body-home-ligth" : "body-home-dark"}>
+            <div className={ themePortfolio ? "quem-sou-ligth" : "quem-sou-dark"}>
+              <h1>Quem sou ?!</h1>
+              <p>Olá , me chamo Matheus Luiz da Silva, tenho 26 anos e sou de São Leopoldo RS.Estudande de Desenvolvimento Web Full-Stack.
+                <br/>Sou um profissional em transição de carreira, cada dia mais dedicado, apaixonado, motivado com a área de tecnologia e desenvolvimento, buscando contribuir positivamente na vida das pessoas.
+              </p>
+            </div>
+            <div>
+              <img src={FOTO2} alt="foto-perfil"/>
+            </div>
+          </div>
+            <hr />
+            <br/>
+            <br/>
+          <div className={themePortfolio ? "hardskills-ligth" : "hardskills-dark"}>
+            <h1>Hard Skills</h1>
+            <a href="a"><VscTools size={40}/></a>
+          </div>
           <CardsHardSkills />
+           <hr/>
+           <br/>
+           <br/>
+           <br/>
           <Footer/>
         </body>
     );
