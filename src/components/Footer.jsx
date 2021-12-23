7c27952c0250d76fda8e48854548b42134caf12a
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:11:10 -0300

    add component footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+export default function Footer() {
+  
+  return (
+    <>
+      <div>
+        <div>
+          <h5>©2021</h5>
+          <h3>Matheus Luiz da Silva
+          </h3>
+        </div>
+      <div>
+       <h1>icons</h1>
+      </div>
+    </div>
+    </>
+  );
+}
