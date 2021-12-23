eb4488ae9d80cd5f1840e66514b3ef7d261d0460
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:09:02 -0300

    add funcoes e classes ao header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GiBedLamp } from "react-icons/gi";
 import '../css/App.css';
 import '../css/header.css';
+import PortfolioContext from '../context/PortfolioContext';
 
 export default function Header() {
+  const { setThemePortfolio, themePortfolio} = useContext(PortfolioContext);
+
+  function handleButtonDark({ target }) {
+    setThemePortfolio(!themePortfolio);
+  }
+
   return (
-    <header>
-      <div>
+    <header className={ themePortfolio ? "header-container-ligth" : "header-container-dark"}>
+      <div className={themePortfolio ? "header-ligth" : "header-dark"}>
         <h1>Math</h1>
         <p>.</p>
         <button 
+          className={themePortfolio ? "button-select-ligth" : "button-select-dark"}
           type='button'
+          onClick={ (target) => handleButtonDark(target) }
         >
           <GiBedLamp size={20} />
         </button>
         <input type="checkbox" id="toggle"/>
           <label class="checkbox" for="toggle">
-           
+            <div class={themePortfolio ? "trace-ligth" : "trace-dark"}></div>
+            <div class={themePortfolio ? "trace-ligth" : "trace-dark"}></div>
+            <div class={themePortfolio ? "trace-ligth" : "trace-dark"}></div>
           </label>
 
         <div class="menu"></div>
-          <nav>
+          <nav class="menu-items">
             <ul>
               <li><a href="/">Home</a></li>
               <li><a href="/projetos">Projetos</a></li>
