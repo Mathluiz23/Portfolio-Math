7c7f43909bd074494e110aaf4f5146c7b440b981
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:17:41 -0300

    add restante dos cards das hardskills

--- src/components/CardsHardSkills.jsx
+++ src/components/CardsHardSkills.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
-import { SiReact } from "react-icons/si";
-
+import { SiRedux, SiReact, SiMysql} from "react-icons/si";
+import { DiJavascript } from "react-icons/di";
+import { GrDocker } from "react-icons/gr";
+import { FaGitAlt, FaGithub, FaHtml5, FaCss3Alt } from "react-icons/fa";
 import PortfolioContext from '../context/PortfolioContext';
 import '../css/App.css';
 
@@ -13,6 +15,51 @@ export default function CardHardSkills() {
         <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
           <a href="https://pt-br.reactjs.org/docs/getting-started.html" target="blank"><SiReact size={100}/></a>
           <h2>React</h2>
+        </div>
+
+        <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
+          <a href="https://redux.js.org/" target="blank"><SiRedux size={100}/></a>
+          <h2>React Redux</h2>
+        </div>
+
+        <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
+          <a  href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript" target="blank"><DiJavascript size={100}/></a>
+          <h2>JavaScript</h2>
+        </div>
+
+        <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
+          <a href="https://git-scm.com/docs/git/pt_BR" target="blank"><FaGitAlt size={100}/></a>
+          <h2>Git</h2>  
+        </div>
+
+        <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
+          <a href="https://docs.github.com/pt" target="blank"><FaGithub size={100}/></a>
+          <h2>GitHub</h2>  
+        </div>
+
+        <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
+          <a href="https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element" target="blank"><FaHtml5 size={100}/></a>
+          <h2>Html 5</h2>  
+        </div>
+        
+        <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
+          <a href="https://developer.mozilla.org/pt-BR/docs/Web/CSS" target="blank"><FaCss3Alt size={100}/></a>
+          <h2>CSS 3</h2>
+        </div>
+
+        <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
+          <a href="https://docs.docker.com/" target="blank"><GrDocker size={100}/></a>
+          <h2>Docker</h2>
+        </div>
+
+        <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
+          <a href="https://dev.mysql.com/doc/" target="blank"><SiMysql size={100}/></a>
+          <h2>MySQL</h2>
+        </div>
+
+        <div className={ themePortfolio ? "hardSkills-card-ligth" :"hardSkills-card-dark"}>
+          <a  href="https://pt-br.reactjs.org/docs/hooks-reference.html" target="blank"><SiReact size={100}/></a>
+          <h2>Hooks + Context</h2>
         </div>
       </div>
     </>
