9d3418a2afe6323fa60b3bde6ad80fcdee125385
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:20:56 -0300

    add component de lista de projetos

---
+++ src/components/ProjetosList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import '../css/App.css';
+
+export default function ProjetosList() {
+   
+    return (
+        <div>
+
+          <div className="card-container">
+            <div className="card">
+              <div>
+                <h2>Projeto 1</h2>
+                <p>Vem ver!</p>
+              </div>
+              
+            </div>
+            <div>
+              Aqui contém um projeto realizado na Trybe, no curso de desenvolvimento Web. Colocando em prática os conhecimentos adquiridos.
+            </div> 
+          </div>
+        </div> 
+    );
+}
