dac663e353c0bd89408718d8f34e6975df0a4545
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:30:24 -0300

    inserindo  projetos page

---
+++ src/components/Pages/Projetos.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/App.css';
+
+
+export default function Projetos() {
+  
+    return (
+        <>
+          <body>
+           
+          </body>
+        </>
+    );
+}
