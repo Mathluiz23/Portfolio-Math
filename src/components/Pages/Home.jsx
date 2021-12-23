297507a2661a4ea516e42fa375c22c4d8b944724
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2021-12-23 18:27:20 -0300

    inserindo context na home

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../css/App.css';
 
 export default function Home() {
+  const { themePortfolio } = useContext(PortfolioContext);
+  console.log(themePortfolio);
     return (
         <body>
      
