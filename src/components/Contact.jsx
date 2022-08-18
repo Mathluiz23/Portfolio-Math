b15cb311bb9517cff40cf662732158522cdc091a
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-08-18 18:57:20 -0300

    Implementando refatoração, inserindo arrays de objetos e realizando um map para renderizar os contatos

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,41 +10,62 @@ import { BsTelegram } from 'react-icons/bs';
 
 export default function Contact() {
   const { themePortfolio } = useContext(PortfolioContext);
+
+  const contact = [
+    { 
+      text: 'Send me a whatsapp',
+      icon: <RiWhatsappFill size={40}/>,
+      link: "https://web.whatsapp.com/send?phone=353832072425"
+    },
+    { 
+      text: 'mathluizs23@gmail.com',
+      icon: <HiOutlineMail size={40}/>,
+      link: "mailto:mathluizs23@gmail.com"
+    },
+  ];
+
+  const infoContact = [
+    { 
+      text: 'Waterford - IE',
+      icon: <FaMapMarkerAlt size={40}/>, 
+      link: "",
+    },
+    { 
+      text: '(+353)83207-2425', 
+      icon: <BsFillTelephoneFill size={40}/>, 
+      link: "https://web.whatsapp.com/send?phone=353832072425"},
+    { 
+      text: 'Telegram',
+      icon: <BsTelegram size={40}/>,
+      link: ""
+    },
+  ];
+
   return (
     <>
       <div className={ themePortfolio ? "entre-em-contato-ligth" :"entre-em-contato-dark"}>
         <h1>Contact me</h1>
         <h1 className="title-contato">!</h1>
       </div>
-
       <div className="contact-me">
         <div className={ themePortfolio ? "contato-ligth" :"contato-dark"}>
           <div className={ themePortfolio ? "social-contact-ligth" : "social-contact-dark"}>
-            <div className="contact">
-              <a href="https://web.whatsapp.com/send?phone=353832072425" target="blank"><RiWhatsappFill size={40}/></a>
-              <h3>Send me a whatsapp</h3>
-            </div>
-
-            <div className="contact">
-              <a href="mailto:mathluizs23@gmail.com" target="blank"><HiOutlineMail size={40}/></a>
-              <h3>mathluizs23@gmail.com</h3>
-            </div>
+            {contact.map(({icon, text, link}) =>
+              <div className="contact">
+                <a href={link} target="blank">{icon}</a>
+                <h3>{text}</h3>
+              </div>
+            )}
           </div>
         </div>
 
         <div className={ themePortfolio ? "contact-local-ligth" : "contact-local-dark"}>
-          <div className="contact">
-            <a href="a" target="blank"><FaMapMarkerAlt size={40}/></a>
-            <h3>Waterford - IE</h3>
-          </div>
-          <div className="contact">
-            <a href="https://web.whatsapp.com/send?phone=5551981386882" target="blank"><BsFillTelephoneFill size={35}/></a>
-            <h3>(+353)83207-2425</h3>
-          </div>
-          <div className="contact">
-            <a href="a" target="blank"><BsTelegram size={40}/></a>
-            <h3>Telegram</h3>
-          </div>
+        {infoContact.map(({text, icon, link}) =>
+            <div className="contact">
+              <a href={link} target="blank">{icon}</a>
+              <h3>{text}</h3>
+            </div>
+        )}
         </div>
       </div>
     </>
